f715add2bdaa785ad7769b973e02647a5345c568
chiyongs <tlscldyd@gmail.com>
2022-12-08 21:01:26 +0900

    feat: 간단 배포 스크립트 추가, 추후 추가 수정 예정정

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,21 @@
-echo "==== [Build] SAMMaruClient ===="
+# 간단하게 짜놓은 배포 스크립트
+# 추후 수정 예정
+
+echo "fetching..."
 
 cd ./SAMMaruClient
+git pull
 
-echo "[SAMMaruClient] Installing dependencies"
-npm install
+cd ../SAMMaruServer
+git pull
 
-echo "[SAMMaruClient] Building"
-npm run build
+echo "docker compose down"
+docker-compose down
+
+echo "docker compose up"
+cd ..
+docker-compose -f docker-compose-prod.yml up --build -d
+
+
 
 
