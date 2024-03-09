ce2f65627a6e87ce2a3bc649661076ac22f60add
chiyongs <tlscldyd@gmail.com>
2024-03-09 15:09:01 +0900

    feat : 배포 스크립트 수정

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,21 @@
 # 간단하게 짜놓은 배포 스크립트
 # 추후 수정 예정
-
+#!/bin/bash
+script_dir=$(cd $(dirname "${BASH_SOURCE[0]}") &> /dev/null && pwd)
 echo "fetching..."
-
-cd ./SAMMaruClient
+echo "${script_dir}"
+cd ${script_dir}/SAMMaruClient
 git pull
 
-cd ../SAMMaruServer
+cd ${script_dir}/SAMMaruServer
 git pull
 
+cd ${script_dir}
 echo "docker compose down"
 docker-compose down
 
 echo "docker compose up"
-cd ..
 docker-compose -f docker-compose-prod.yml up --build -d
-
 
 
 
