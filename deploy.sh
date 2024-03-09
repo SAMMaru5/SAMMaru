36f5d99f09bb5a78d704d1e9cd5a5940b302c1d5
chiyongs <tlscldyd@gmail.com>
2024-03-09 16:13:43 +0900

    feat : shebang 라인 이동

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
+#!/bin/bash
 # 간단하게 짜놓은 배포 스크립트
 # 추후 수정 예정
-#!/bin/bash
 script_dir=$(cd $(dirname "${BASH_SOURCE[0]}") &> /dev/null && pwd)
 echo "fetching..."
 echo "${script_dir}"
--- stop.sh
+++ stop.sh
@@ -1,6 +1,6 @@
+#!/bin/sh
 # 간단하게 짜놓은 배포 스크립트
 # 추후 수정 예정
-#!/bin/bash
 script_dir=$(cd $(dirname "${BASH_SOURCE[0]}") &> /dev/null && pwd)
 echo "stop..."
 echo "${script_dir}"
